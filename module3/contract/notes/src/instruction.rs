5b3bd91fc7b802ba7391d7e27466a3737fdf82d6
Davirain <davirain.yin@gmail.com>
2023-07-13 12:02:22 +0800

    move to instruction.rs

---
+++ module3/contract/notes/src/instruction.rs
@@ -0,0 +1,55 @@
+use borsh::BorshDeserialize;
+use solana_program::program_error::ProgramError;
+
+pub enum NoteInstruction {
+    CreateNote {
+        title: String,
+        body: String,
+        id: u64,
+    },
+    UpdateNote {
+        title: String,
+        body: String,
+        id: u64,
+    },
+    DeleteNote {
+        id: u64,
+    },
+}
+
+#[derive(BorshDeserialize)]
+pub struct NoteInstructionPayload {
+    pub id: u64,
+    pub title: String,
+    pub body: String,
+}
+
+impl NoteInstruction {
+    // Unpack inbound buffer to associated Instruction
+    // The expected format for input is a Borsh serialized vector
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        // Take the first byte as the variant to
+        // determine which instruction to execute
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+        // Use the temporary payload struct to deserialize
+        let payload = NoteInstructionPayload::try_from_slice(rest).unwrap();
+        // Match the variant to determine which data struct is expected by
+        // the function and return the TestStruct or an error
+        Ok(match variant {
+            0 => Self::CreateNote {
+                title: payload.title,
+                body: payload.body,
+                id: payload.id,
+            },
+            1 => Self::UpdateNote {
+                title: payload.title,
+                body: payload.body,
+                id: payload.id,
+            },
+            2 => Self::DeleteNote { id: payload.id },
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
