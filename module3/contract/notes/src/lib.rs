2add38f589084c689f2f393e5d70fe193e1f153a
Davirain <davirain.yin@gmail.com>
2023-07-13 11:57:29 +0800

    move hello-world-frontend to fronted folds

--- module3/contract/notes/src/lib.rs
+++ module3/contract/notes/src/lib.rs
@@ -1,9 +1,33 @@
+use borsh::BorshDeserialize;
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
 // program id: EHiYp5VkjpU6Pf36C8BgSxQLkTqVuUPKtkPpyzEqbiga
 entrypoint!(process_instruction);
+
+pub enum NoteInstruction {
+    CreateNote {
+        title: String,
+        body: String,
+        id: u64,
+    },
+    UpdateNote {
+        title: String,
+        body: String,
+        id: u64,
+    },
+    DeleteNote {
+        id: u64,
+    },
+}
+
+#[derive(BorshDeserialize)]
+pub struct NoteInstructionPayload {
+    pub id: u64,
+    pub title: String,
+    pub body: String,
+}
 
 pub fn process_instruction(
     _program_id: &Pubkey,
