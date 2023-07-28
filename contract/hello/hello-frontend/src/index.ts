df3b3d642bb64bf13d166295290f5a90b6e8675a
Davirain <davirain.yin@gmail.com>
2023-07-28 18:29:25 +0800

    add raw solana hello-world program

---
+++ contract/hello/hello-frontend/src/index.ts
@@ -0,0 +1,97 @@
+// We're adding these
+import * as Web3 from '@solana/web3.js';
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PROGRAM_ID = new Web3.PublicKey("SexbzUwxknP3Qvfq2ySMLNu1jqt9Tys7NNDAs3tHTep")
+
+async function main() {
+  const connection = new Web3.Connection("http://localhost:8899", 'confirmed');
+  const signer = await initializeKeypair();
+
+  // When generating a keypair
+  await airdropSolIfNeeded(signer, connection);
+
+  console.log("Public key:", signer.publicKey.toBase58());
+
+  await helloWrold(connection, signer);
+}
+
+main()
+  .then(() => {
+    console.log("Finished successfully")
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
+
+async function initializeKeypair(): Promise<Web3.Keypair> {
+  if (!process.env.PRIVATE_KEY) {
+    console.log('Generating new keypair... 🗝️');
+    const signer = Web3.Keypair.generate();
+
+    console.log('Creating .env file');
+    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+
+    return signer;
+  }
+
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+  return keypairFromSecret;
+}
+
+async function airdropSolIfNeeded(
+  signer: Web3.Keypair,
+  connection: Web3.Connection
+) {
+  const balance = await connection.getBalance(signer.publicKey);
+  console.log('Current balance is', balance / Web3.LAMPORTS_PER_SOL, 'SOL');
+
+  // 1 SOL should be enough for almost anything you wanna do
+  if (balance / Web3.LAMPORTS_PER_SOL < 1) {
+    // You can only get up to 2 SOL per request
+    console.log('Airdropping 1 SOL');
+    const airdropSignature = await connection.requestAirdrop(
+      signer.publicKey,
+      Web3.LAMPORTS_PER_SOL
+    );
+
+    const latestBlockhash = await connection.getLatestBlockhash();
+
+    await connection.confirmTransaction({
+      blockhash: latestBlockhash.blockhash,
+      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+      signature: airdropSignature,
+    });
+
+    const newBalance = await connection.getBalance(signer.publicKey);
+    console.log('New balance is', newBalance / Web3.LAMPORTS_PER_SOL, 'SOL');
+  }
+}
+
+async function helloWrold(connection: Web3.Connection, payer: Web3.Keypair) {
+  const transaction = new Web3.Transaction()
+  const instruction = new Web3.TransactionInstruction({
+    // Instructions need 3 things
+
+    // 1. The public keys of all the accounts the instruction will read/write
+    keys: [],
+
+    // 2. The ID of the program this instruction will be sent to
+    programId: PROGRAM_ID
+
+    // 3. Data - in this case, there's none!
+  })
+
+  transaction.add(instruction)
+  const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+
+  console.log(
+    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
